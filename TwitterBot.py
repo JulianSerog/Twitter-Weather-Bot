23c44b7f58ecdb88823cc506dbf22783ad0200dd
Julian (Esa) Serog <esa5959@yahoo.com>
2016-02-22 00:44:50 -0500

    updated code and using pywapi for weather framework

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #import frameworks
-import tweepy, time, sys
+import tweepy, time, sys, pywapi
 
 CONSUMER_KEY = 'zOqCHqDWjhcqnxl2DWd4QBtsX'
 CONSUMER_SECRET = 'hjXtgqS40u9paLEw99Wlp1PKrbOeGAF27KGQ4Yx2gEfg40Q4uk'
@@ -10,6 +10,9 @@ ACCESS_SECRET = 'ILJpeb7VFnm98HBdZCHglysHkRvCW3bS2QL5MR3kX6iLs'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+
+
 
 
 """
@@ -28,3 +31,12 @@ for line in f:
     api.update_status(status=line)
     time.sleep(15)#Tweet each line in file every 15 seconds
 """
+
+#create an infinite loop that updates the status to whatever the weather is currently
+while True:
+	#set a string here based on weather
+	weather_com_result = pywapi.get_weather_from_weather_com("30605")
+	message = "It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Athens, Georgia.\n"
+
+	api.update_status(message)
+	time.sleep(3600) #sleep for 1 hour
