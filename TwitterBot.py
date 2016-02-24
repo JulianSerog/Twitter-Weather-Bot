a2a4c98d6bf47ebaf9e610ef2772f602c25e4209
Julian (Esa) Serog <esa5959@yahoo.com>
2016-02-24 00:24:24 -0500

    added try catch for error handling

--- TwitterBot.py
+++ TwitterBot.py
@@ -33,18 +33,28 @@ previousTweet = ""
 while True:
 	#print pywapi.get_location_ids("Athens") #gets all location ID's for 'athens', prints to console, I used this to find the code for Athens, Georgia
 	yahoo_result = pywapi.get_weather_from_yahoo('USGA0027') #Athens Georgia code
+	#update new tweet text
 	message = str(yahoo_result['condition']['text']).lower() + " and " + str(yahoo_result['condition']['temp']) + "C now in Athens, Georgia.\n"
 	#to grab first status in tweepy.Cursor
 	counter = 0 
 	#find a more efficient way of doing this, tweepy documentation is tricky but this should solve the issue for now
 	for status in tweepy.Cursor(api.user_timeline).items():
 		if counter == 0:
-			print "previousTweet: " + status.text
+			previousTweet = status.text
+			print "previous: " + previousTweet
 			break
-	
-	print "current weather to tweet: " + message
+		counter += 1
+	#print statements for error checking
+	print "current: " + message.strip(" ")
 	#only tweet when weather has changed
-	if message != previousTweet:
-		api.update_status(message)
-		print "new tweet posted"
-	time.sleep(3600) #sleep for 1 hour
+	print "old tweet == new tweet: "
+	print message.strip(" ") == previousTweet.strip(" ")
+	if str(message) != str(previousTweet):
+		try:
+			api.update_status(message)
+			print "new tweet posted"
+		except:
+			print "tweet matched an old tweet, waiting 30 minutes to attempt to update..."
+			pass
+
+	time.sleep(1800) #sleep for 30
