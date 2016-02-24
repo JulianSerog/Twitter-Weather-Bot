fb3e23a62cd677522ba259645e254fbd9e038bda
Julian (Esa) Serog <esa5959@yahoo.com>
2016-02-23 22:05:11 -0500

    fixed a bug where if a new tweet's text matched the last tweet a crash would occur

--- TwitterBot.py
+++ TwitterBot.py
@@ -11,10 +11,6 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-
-
-
-
 """
 #USED FOR TESTING WITH READING IN FILES
 #grab file from command line arguements
@@ -32,12 +28,23 @@ for line in f:
     time.sleep(15)#Tweet each line in file every 15 seconds
 """
 
-
+previousTweet = ""
 #create an infinite loop that updates the status to whatever the weather is currently
 while True:
-	#set a string here based on weather
-	print pywapi.get_location_ids("Athens") #gets all location ID's for 'athens', prints to console, I used this to find the code for Athens, Georgia
+	#print pywapi.get_location_ids("Athens") #gets all location ID's for 'athens', prints to console, I used this to find the code for Athens, Georgia
 	yahoo_result = pywapi.get_weather_from_yahoo('USGA0027') #Athens Georgia code
 	message = str(yahoo_result['condition']['text']).lower() + " and " + str(yahoo_result['condition']['temp']) + "C now in Athens, Georgia.\n"
-	api.update_status(message)
+	#to grab first status in tweepy.Cursor
+	counter = 0 
+	#find a more efficient way of doing this, tweepy documentation is tricky but this should solve the issue for now
+	for status in tweepy.Cursor(api.user_timeline).items():
+		if counter == 0:
+			print "previousTweet: " + status.text
+			break
+	
+	print "current weather to tweet: " + message
+	#only tweet when weather has changed
+	if message != previousTweet:
+		api.update_status(message)
+		print "new tweet posted"
 	time.sleep(3600) #sleep for 1 hour
